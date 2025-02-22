1462b61bd1073085ddaa274be20d3da2f8427dd9
trstnb1998 <tristanbartolome@hotmail.com>
2025-02-22 12:58:44 +1100

    FizzBuzz practice

---
+++ Exercises/FizzBuzz.py
@@ -0,0 +1,11 @@
+# FizzBuzz practice
+
+for i in range(1, 100):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
